6801bc62b9cf4342f9f83ff4be19c3d3d7ee3082
Evgeny Tomsen <eugene.tomsen@gmail.com>
2019-12-17 17:19:17 +0100

    feature(ctx-menu): add context menu, add text on edges

--- app/components/graph.js
+++ app/components/graph.js
@@ -3,6 +3,8 @@ import go from 'gojs';
 
 function mapEdgeToGo(e) {
   return {
+    font: `${e.fontSize}px serif`,
+    text: e.text,
     to: e.end,
     from: e.start
   };
@@ -26,12 +28,29 @@ function mapVertexToGo(v) {
 }
 
 export default Component.extend({
+
+  willDestroyElement() {
+    this.graph.destroy();
+    this._super(...arguments);
+  },
+  
   didInsertElement() {
-    const onChange = this.onChange;
+    const contextMenuElement = this.createContextMenu();
+    this.set('contextMenuElement', contextMenuElement);
+
+    const graph = this.createGraph();
+    this.set('graph', graph);
+  },
+
+  createGraph() {
     const Graph = go.GraphObject.make;
     const graph = Graph(go.Diagram, this.elementId);
     const nodeData = this.vertices.map(mapVertexToGo);
     const linkData = this.edges.map(mapEdgeToGo);
+    const contextMenu = Graph(go.HTMLInfo, {
+      show: this.showContextMenu.bind(this),
+      hide: this.hideContextMenu.bind(this),
+    });
 
     const node = Graph(
       go.Shape,
@@ -48,7 +67,10 @@ export default Component.extend({
     graph.toolManager.linkingTool.temporaryLink = Graph(
       go.Link,
       { layerName: 'Tool' },
-      Graph(go.Shape, { stroke: 'red', strokeWidth: 2, strokeDashArray: [4, 2]})
+      Graph(
+        go.Shape,
+        { stroke: 'red', strokeWidth: 2, strokeDashArray: [4, 2]}
+      )
     );
 
     const nodeText = Graph(
@@ -59,37 +81,76 @@ export default Component.extend({
       new go.Binding('text', 'text'),
       new go.Binding('font', 'font'),
     );
+
     graph.nodeTemplate = Graph(
       go.Node,
       'Auto',
+      { contextMenu },
       node,
       nodeText
     );
+
     graph.linkTemplate = Graph(
       go.Link,
       {
         relinkableFrom: true,
-        relinkableTo: true
+        relinkableTo: true,
+        contextMenu
       },
       Graph(go.Shape),
-      Graph(go.Shape, { toArrow: 'Standard' })
+      Graph(
+        go.Shape,
+        { toArrow: 'Standard' }
+      ),
+      Graph(
+        go.TextBlock,
+        new go.Binding('text', 'text'),
+        new go.Binding('font', 'font')
+      )
     );
     graph.model = new go.GraphLinksModel(nodeData, linkData);
-    graph.addModelChangedListener(function(event) {
-      if (!event.isTransactionFinished) {
-        return;
-      }
-      if (event.oldValue === 'Initial Layout') {
-        // skip first change
-        return;
-      }
-      onChange && onChange(event.model);
-    });
-    this.graph = graph;
+    graph.addModelChangedListener(this.onModelChanged.bind(this));
+    return graph;
   },
 
-  willDestroyElement() {
-    this.graph.destroy();
-    this._super(...arguments);
+  showContextMenu(obj, diagram, tool) {
+    // var cmd = diagram.commandHandler;
+    this.contextMenuElement.classList.add('graph__menu--active');
+    const {x, y} = diagram.lastInput.viewPoint;
+    this.contextMenuElement.style.left = `${x}px`;
+    this.contextMenuElement.style.top = `${y}px`;
+    window.addEventListener('click', this.onWindowClick.bind(this), true);
+  },
+
+  hideContextMenu() {
+    this.contextMenuElement.classList.remove('graph__menu--active');
+    window.removeEventListener('click', this.onWindowClick.bind(this), true);
+  },
+
+  onWindowClick() {
+    if (this.graph.currentTool instanceof go.ContextMenuTool) {
+      this.graph.currentTool.doCancel();
+    }
+  },
+
+  createContextMenu() {
+    const contextMenuElement = document.getElementById('contextMenu');
+    contextMenuElement.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+      return false;
+    }, false);
+    return contextMenuElement;
+  },
+
+
+  onModelChanged(event) {
+    if (!event.isTransactionFinished) {
+      return;
+    }
+    if (event.oldValue === 'Initial Layout') {
+      // skip first change
+      return;
+    }
+    this.onChange && this.onChange(event.model);
   }
 });
